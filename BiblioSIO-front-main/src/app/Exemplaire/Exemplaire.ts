1996e65156b79a4b42369acd734eab91ed7d0e7a
Maxime ORIOT <moriot@geiffel.fr>
2023-11-10 14:33:19 +0100

    initial push

---
+++ BiblioSIO-front-main/src/app/Exemplaire/Exemplaire.ts
@@ -0,0 +1,11 @@
+import {SerializedUrlObject} from "../utils/SerializedUrlObject";
+
+export interface Exemplaire {
+  id: Number,
+  revue: SerializedUrlObject,
+  titre: String,
+  moisParution: String,
+  anneeParution: String,
+  articles: SerializedUrlObject[],
+  statut: Boolean
+}
